18a77a52e795067a351920e6dc115b05882e1a20
twice9 <jesseta.thach@my.jcu.edu.au>
2019-08-20 09:39:04 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,21 @@
 """
 Jesseta Thach
 """
+
+min_password_length = 6
+
+
+def main():
+    password = input("Enter your password: ")
+    while len(password) < min_password_length:
+        print("Invalid password...")
+        password = input("Enter your password: ")
+    print('*' * len(password))
+
+
+def valid_password(password):
+    if len(password) < min_password_length:
+        return False
+
+
+main()
